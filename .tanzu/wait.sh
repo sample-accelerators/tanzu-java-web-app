d57143b1ccff42cb6bba9563d66b351cc7234cf6
Paul Warren <warrenpa@vmware.com>
2021-09-19 23:26:13 -0700

    Refactor wait command to use imagerepository source url
    - requires inner loop supply chain [PR #64](https://github.com/vmware-tanzu-private/inner-loop-supplychain/pull/64)

--- .tanzu/wait.sh
+++ .tanzu/wait.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 
 workload=$1
-while [[ -z $(kubectl get pod -o jsonpath='{.items[?(@.metadata.annotations.developer\.apps\.tanzu\.vmware\.com/image-source-digest=="'$(kubectl get workload ${workload} -o=jsonpath='{.spec.source.image}')'")].metadata.name}') ]]; do echo "waiting..." && sleep 10; done 
+while [[ -z $(kubectl get pod -o jsonpath='{.items[?(@.metadata.annotations.developer\.apps\.tanzu\.vmware\.com/image-source-digest=="'$(kubectl get imagerepository ${workload}-source -o=jsonpath='{.status.url}')'")].metadata.name}') ]]; do echo "waiting..." && sleep 10; done 
 kubectl wait pod --for=condition=ready -l carto.run/workload-name=$workload,app.kubernetes.io/part-of=$workload
