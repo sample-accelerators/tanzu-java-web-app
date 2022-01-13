d0c687eb3855bddbc85c76135f27431897261cd1
Mike Nelson Jr <michaeljr.nelson@gmail.com>
2022-01-13 11:14:41 -0500

    Update HelloController.java
    Update message

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu!";
+		return "Hello from Spring Boot + Tanzu!";
 	}
 
 }
