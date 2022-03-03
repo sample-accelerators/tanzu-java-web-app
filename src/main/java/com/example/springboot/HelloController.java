b48b4b429289818987b8fbfe468a79dcb0e3217a
Gowtham Shankar <gowthamshankar@sgowthamsha-a01.vmware.com>
2022-03-03 12:14:17 -0500

    testing done

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! - Gowtham's Version 3";
+		return "Greetings from Spring Boot + Tanzu! - Gowtham's Version 32";
 	}
 
 }
