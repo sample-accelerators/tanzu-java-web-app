576fa95c12d48d4e8295c30fbb1d624b7e0133f4
Gowtham Shankar <gowthamshankar@sgowthamsha-a01.vmware.com>
2022-04-08 11:54:34 -0400

    test changes

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu!", controller.index());
+        assertEquals("Greetings from Spring Boot + Tanzu from SE Comm Team", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu!"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu from SE Comm Team"));
     }
 }
